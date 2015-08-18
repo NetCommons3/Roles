6314f8721c9ed0de8ac8c0b2189b334c0268f3d3
s-nakajima <nakajimashouhei@gmail.com>
2015-08-18 18:24:59 +0900

    ルーム管理の開発

--- Model/Role.php
+++ Model/Role.php
@@ -37,10 +37,23 @@ class Role extends RolesAppModel {
  *
  * @var const
  */
+	//後で、ROOM_ROLE_KEYに置き換える
 	const
 		ROLE_KEY_SYSTEM_ADMINISTRATOR = 'system_administrator',
 		ROLE_KEY_EDITOR = 'editor',
 		ROLE_KEY_VISITOR = 'visitor';
+
+/**
+ * Room Role keys
+ *
+ * @var const
+ */
+	const
+		ROOM_ROLE_KEY_ROOM_ADMINISTRATOR = 'room_administrator',
+		ROOM_ROLE_KEY_CHIEF_EDITOR = 'chief_editor',
+		ROOM_ROLE_KEY_EDITOR = 'editor',
+		ROOM_ROLE_KEY_GENERAL_USER = 'general_user',
+		ROOM_ROLE_KEY_VISITOR = 'visitor';
 
 /**
  * Validation rules
