1aed78f2c0c4b615d1a20d855e2de1313d34f345
s-nakajima <nakajimashouhei@gmail.com>
2016-10-09 09:08:58 +0900

    DefaultRolePermissionFixtureのデフォルト値修正

--- Test/Fixture/DefaultRolePermissionFixture.php
+++ Test/Fixture/DefaultRolePermissionFixture.php
@@ -395,6 +395,9 @@ class DefaultRolePermissionFixture extends CakeTestFixture {
 		require_once App::pluginPath('Roles') . 'Config' . DS . 'Schema' . DS . 'schema.php';
 		$this->fields = (new RolesSchema())->tables[Inflector::tableize($this->name)];
 
+		require_once App::pluginPath('Roles') . 'Config' . DS . 'Migration' . DS . '1469603399_records.php';
+		$this->records = (new RolesRecords())->records[$this->name];
+
 		parent::init();
 	}
 
